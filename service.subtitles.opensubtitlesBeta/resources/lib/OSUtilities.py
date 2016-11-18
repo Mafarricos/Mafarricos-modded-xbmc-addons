3712dbdf7c419c0661d2c23b8339271e4c1517bd
Mafarricos <MafaStudios@gmail.com>
2016-11-18 16:08:00 +0000

    update on opensubtitles

--- service.subtitles.opensubtitlesBeta/resources/lib/OSUtilities.py
+++ service.subtitles.opensubtitlesBeta/resources/lib/OSUtilities.py
@@ -12,7 +12,7 @@ import unicodedata
 
 __addon__      = xbmcaddon.Addon()
 __version__    = __addon__.getAddonInfo('version') # Module version
-__scriptname__ = "XBMC Subtitles"
+__scriptname__ = "XBMC Subtitles Unofficial"
 
 BASE_URL_XMLRPC = u"http://api.opensubtitles.org/xml-rpc"
 
